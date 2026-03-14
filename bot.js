6b58720f2ff2d96e5f50098583e2efdb18d528e1
S-svg-ui <shreyassingh4536@gmail.com>
2026-03-14 12:45:01 +0530

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,35 @@
+const mineflayer = require('mineflayer')
+
+function startBot() {
+
+  const bot = mineflayer.createBot({
+    host: 'mumumelelo.falix.gg',
+    port: 25565,
+    username: 'AFK_24_7',
+    version: "1.21.1"
+    auth: false
+  })
+
+  bot.on('spawn', () => {
+    console.log("✅ Joined server")
+
+    setInterval(() => {
+      bot.swingArm()
+    }, 45000)
+  })
+
+  bot.on('resourcePack', () => {
+    console.log("📦 Accepting pack")
+    bot.acceptResourcePack()
+  })
+
+  bot.on('end', () => {
+    console.log("❌ Reconnecting in 10s")
+    setTimeout(startBot, 10000)
+  })
+
+  bot.on('kicked', console.log)
+  bot.on('error', console.log)
+}
+
+startBot()
